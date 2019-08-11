0578ba6673de1f480008dd29df5b0e207581c9e1
alspok <alspok@gmail.com>
2019-08-11 22:44:26 +0300

    DinucTable added

---
+++ Models/DinucTableModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DinucMVC.Models
+{
+    public class DinucTable
+    {
+
+        public string[] Dinuc { get; set; } = {"aa", "ac", "ag", "at",
+                                               "ca", "cc", "cg", "ct",
+                                               "ga", "gc", "gg", "gt",
+                                               "ta", "tc", "tg", "tt"};
+        public int[] DinucCount { get; set; } = new int[16];
+        public double[] DinucFrq { get; set; } = new double[16];
+    }
+}
